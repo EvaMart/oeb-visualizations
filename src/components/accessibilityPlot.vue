bc5c9eefc05d65b0d1393e277d4104ed5d33f0f0
emarti2 <eva.martin@bsc.es>
2023-11-22 15:25:04 +0100

    legend bug fixed

--- src/components/accessibilityPlot.vue
+++ src/components/accessibilityPlot.vue
@@ -414,7 +414,7 @@ export default {
                 },
                 name: name,
                 type:"bar",
-                legendgroup:'down',
+                legendgroup: name,
                 showlegend: showlegend,
                 hoverinfo: hoverinfo,
                 hovertemplate: hovertemplate,
