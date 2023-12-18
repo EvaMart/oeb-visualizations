bf94cf8145ad4c0b6233fe65c338569ac2cdac28
Eva Martin del Pico <eva.mdpico@gmail.com>
2023-12-18 16:15:37 +0100

    feat: add scatterplot

---
+++ src/components/scatterPlot.vue
@@ -0,0 +1,30 @@
+<template>
+    <div  :id="divId">
+    </div>
+</template>
+<script>
+import Plotly from 'plotly.js-dist'
+import { randstr } from '../utils';
+
+
+export default {
+
+    name: 'ScatterPlot',
+    props: {
+        x: {
+            type: Array,
+            required: true
+        },
+        y: {
+            type: Array,
+            required: true
+        },
+    },
+    data() {
+        return {
+            divId: randstr('scatter_plot_'),
+        }
+    },
+}
+
+</script>
