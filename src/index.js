b122b05aaf4d7bb2067889ba5fccb43be254a881
avalle <ani.valle@bsc.es>
2024-05-09 14:57:46 +0200

    Add BarPlot and  ScatterPlot in index.ja

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import accessibilityPlot from "./components/accessibilityPlot.vue";
 import citationsPlot from "./components/citationsPlot.vue";
+import ScatterPlot from "./components/ScatterPlot.vue";
+import BarPlot from "./components/BarPlot.vue";
 
 export {
   accessibilityPlot,
-  citationsPlot
+  citationsPlot,
+  BarPlot,
+  ScatterPlot
 }
